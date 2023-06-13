8f09921a248e40fb784e085f0d37781c5e3a807c
ansatzX <ansatzMe@outlook.com>
2023-06-13 21:18:14 +0800

    update depend on arm py, and drop conda depend.

--- Formula/mokit.rb
+++ Formula/mokit.rb
@@ -1,35 +1,58 @@
 class Mokit < Formula
     desc "Molecular Orbital KIT"
     homepage "https://gitlab.com/jxzou/mokit"
-    url "https://gitlab.com/jxzou/mokit/-/archive/master/mokit-master.tar.gz"
-    # sha256 "328fec4d07ea84782c9ecf8f2f9ae9f1e38750450e34035301494c62a44ebcc3"
-    version "master"
+    url "https://gitlab.com/jxzou/mokit/-/archive/v1.2.5/mokit-v1.2.5.tar.gz"
+    sha256 "5ffa4df60f48cf4bec41ccde1b388f9440521d0ba07755f854c74df4414acf5d"
+    version "v1.2.5-with-options"
     license " Apache License 2.0"
+    head "https://gitlab.com/jxzou/mokit.git", branch: "master"
+    
+    option "with-py38", "interface between fortran and python@3.8"
+    option "with-py39", "interface between fortran and python@3.9"
+    option "with-py310", "interface between fortran and python@3.10"
+    option "with-py311", "interface between fortran and python@3.11"
 
     on_macos do
 
         depends_on "gcc" => :build
         depends_on "gfortran" => :build
         depends_on "openblas" => :build
-        # depends_on "miniconda" => :build
         depends_on "make" => :build
+        depends_on "coreutils" => :build
+        depends_on "binutils" => :build
+
+        depends_on "python@3.8" if build.with? "py38"
+        depends_on "python@3.9" if build.with? "py39"
+        depends_on "python@3.10" if build.with? "py310"
+        depends_on "python@3.11" if build.with? "py311"
+
+        
       end
   
     def install
-        ENV.deparallelize
-        cd "src" do
-            # system "cp", "#{HOMEBREW_PREFIX}/Library/Taps/ansatzx/homebrew-mokit/tools/Makefile.gnu_openblas_macos",  "."
-            # system "cp", "#{HOMEBREW_PREFIX}/Library/Taps/ansatzx/homebrew-mokit/tools/Makefile.main",  "."
-            system "make", "-f", "Makefile.gnu_openblas_macos_brew", "all"
+        if OS.mac? 
+          ENV.deparallelize
+          args = "exe"
+          if build.with? "py38" or build.with? "py39" or build.with? "py310" or build.with? "py311"
+            args = "all"
+          end 
+
+          system "pip3", "install", "numpy"
+
+          cd "src" do
+              system "cp", "#{HOMEBREW_PREFIX}/Library/Taps/ansatzx/homebrew-mokit/tools/Makefile.gnu_openblas_macos",  "."
+              # system "cp", "#{HOMEBREW_PREFIX}/Library/Taps/ansatzx/homebrew-mokit/tools/Makefile.main",  "."
+              system "make", "-f", "Makefile.gnu_openblas_macos" , "#{args}"
+          end
+          prefix.install Dir["bin"]
+          prefix.install Dir["mokit"]
         end
-        prefix.install Dir["bin"]
-        prefix.install Dir["mokit"]
     end
 
     def caveats
         <<~EOS
           You need to take some manual steps in order to make this formula work:
-            export MOKIT_ROOT="$(brew --prefix)/Cellar/mokit/master"
+            export MOKIT_ROOT="$(brew --prefix mokit)"
             export PATH=$MOKIT_ROOT/bin:$PATH
             export PYTHONPATH=$MOKIT_ROOT:$PYTHONPATH
             export LD_LIBRARY_PATH=$MOKIT_ROOT/mokit/lib:$LD_LIBRARY_PATH
--- README.md
+++ README.md
@@ -3,22 +3,51 @@
 This repository provides package build instructions for [mokit](https://gitlab.com/jxzou/mokit) compatible with the [Homebrew toolchain](https://brew.sh).
 
 ## How do I install these formulae?
-You can install *mokit* by tapping this repository
 
-This repo require you install miniconda via homebrew and install numpy 
+You can install *mokit*  by tapping this repository
+
+### Install Releases with python plugins.
 
 ```
-brew install --cask miniconda
-conda init bash (or zsh ) 
-conda activate base
-pip install numpy
+brew install ansatzx/homebrew-mokit/mokit --with-py39
 ```
 
-Then 
+Or
 
-`brew install ansatzx/homebrew-mokit/mokit`
+```
+brew tap ansatzx/homebrew-mokit
+brew install mokit --with-py39 
+```
 
-Or `brew tap ansatzx/homebrew-mokit` and then `brew install mokit`.
+You have options `--with-pyxxx`, varying from 38 to 311, since m1 chips do not have 37 and lower python. 
+
+If you do not want python part (pyscf) of mokit, do not write this option.
+
+### Install latest Git version (HEAD)
+
+Use option `--HEAD` to get latest commit
+
+```
+brew install mokit --with-py39  --HEAD
+```
+
+It's very suitable for pros.
+
+## Update 
+
+```
+brew update && brew upgrade mokit 
+```
+
+```
+brew update && brew upgrade mokit --fetch-HEAD
+```
+
+## Uninstall
+
+```
+brew uninstall mokit
+```
 
 Available programs are
 
