b0ad4896d8a08d4de1b6e4fe6678dd51805af754
ansatzX <ansatzMe@outlook.com>
2024-12-31 13:31:22 +0800

    update git download and 312 313

--- Formula/mokit.rb
+++ Formula/mokit.rb
@@ -5,12 +5,14 @@ class Mokit < Formula
     sha256 "5ffa4df60f48cf4bec41ccde1b388f9440521d0ba07755f854c74df4414acf5d"
     version "v1.2.6-with-options"
     license " Apache License 2.0"
-    head "https://github.com/1234zou/MOKIT", branch: "master"
+    head "https://github.com/1234zou/MOKIT", using: :git
     
     option "with-py38", "interface between fortran and python@3.8"
     option "with-py39", "interface between fortran and python@3.9"
     option "with-py310", "interface between fortran and python@3.10"
     option "with-py311", "interface between fortran and python@3.11"
+    option "with-py312", "interface between fortran and python@3.12"
+    option "with-py313", "interface between fortran and python@3.13"
 
     on_macos do
 
@@ -20,12 +22,14 @@ class Mokit < Formula
         depends_on "make" => :build
         depends_on "coreutils" => :build
         depends_on "binutils" => :build
+        depends_on "meson" => :build
 
         depends_on "python@3.8" if build.with? "py38"
         depends_on "python@3.9" if build.with? "py39"
         depends_on "python@3.10" if build.with? "py310"
         depends_on "python@3.11" if build.with? "py311"
-
+        depends_on "python@3.12" if build.with? "py312"
+        depends_on "python@3.13" if build.with? "py313"
         
       end
   
@@ -33,25 +37,26 @@ class Mokit < Formula
         if OS.mac? 
           ENV.deparallelize
           args = "exe"
-          if build.with? "py38" or build.with? "py39" or build.with? "py310" or build.with? "py311"
+          if build.with? "py38" or build.with? "py39" or build.with? "py310" or build.with? "py311" or build.with? "py312" or build.with? "py313"
             args = "all"
           end 
-
-          system "pip3", "install", "numpy"
+          
+          if not build.with? "py313"
+            system "pip3", "install", "numpy==1.26"
 
           cd "src" do
-              if Hardware::CPU.arm?
-                system "cp", "#{HOMEBREW_PREFIX}/Library/Taps/ansatzx/homebrew-mokit/tools/Makefile.gnu_openblas_macos",  "."
-              end
-              if Hardware::CPU.intel?
-                system "cp", "#{HOMEBREW_PREFIX}/Homebrew/Library/Taps/ansatzx/homebrew-mokit/tools/Makefile.gnu_openblas_macos",  "."
-              end
-              # system "cp", "#{HOMEBREW_PREFIX}/Library/Taps/ansatzx/homebrew-mokit/tools/Makefile.main",  "."
-              system "make", "-f", "Makefile.gnu_openblas_macos" , "#{args}"
-          end
-          prefix.install Dir["bin"]
-          prefix.install Dir["mokit"]
+            if Hardware::CPU.arm?
+              system "cp", "#{HOMEBREW_PREFIX}/Library/Taps/ansatzx/homebrew-mokit/tools/Makefile.gnu_openblas_macos",  "."
+            end
+            if Hardware::CPU.intel?
+              system "cp", "#{HOMEBREW_PREFIX}/Homebrew/Library/Taps/ansatzx/homebrew-mokit/tools/Makefile.gnu_openblas_macos",  "."
+            end
+            # system "cp", "#{HOMEBREW_PREFIX}/Library/Taps/ansatzx/homebrew-mokit/tools/Makefile.main",  "."
+            system "make", "-f", "Makefile.gnu_openblas_macos" , "#{args}"
         end
+        prefix.install Dir["bin"]
+        prefix.install Dir["mokit"]
+      end
     end
 
     def caveats
